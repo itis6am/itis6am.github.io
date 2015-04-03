07bd5f20fc0498e05778edf9e9e0753297be20be
Richard Lee <lirui.sep@gmail.com>
2015-04-04 00:29:02 +0800

    landingpage

---
+++ download/qcrl.js
@@ -0,0 +1,18 @@
+/**
+	支持按渠道下载
+**/	
+	var apkURL = "http://www.itis6am.com/upload/android/qcrl.apk"; 
+    
+	// var channel = "";
+        
+	// function getURLParameters(key){
+	//     return (document.location.search.match(new RegExp("(?:^\\?|&)"+key+"=(.*?)(?=&|$)"))||['',null])[1];
+	// }
+
+ //     channel = getURLParameters("channel");
+ //     if(channel == null) {
+ //     	channel = "";
+ //     }   
+ //     if(channel != null && channel.length > 0 ) {
+ //         apkURL = "http://hlj-app.b0.upaiyun.com/zmw/upload/android-package/app/" + channel + "/helijia.apk";
+ //     }
