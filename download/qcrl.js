d797ffc6fc2337b50b151442167b11631e156947
Richard Lee <lirui.sep@gmail.com>
2015-08-18 17:13:49 +0800

    下载区分渠道

--- download/qcrl.js
+++ download/qcrl.js
@@ -1,18 +1,21 @@
 /**
 	支持按渠道下载
 **/	
-	var apkURL = "http://cdnfile.itis6am.com/qcrl.apk"; 
+	// var apkURL = "http://cdnfile.itis6am.com/qcrl.apk"; 
     
-	// var channel = "";
+	var channel = "";
         
-	// function getURLParameters(key){
-	//     return (document.location.search.match(new RegExp("(?:^\\?|&)"+key+"=(.*?)(?=&|$)"))||['',null])[1];
-	// }
+	function getURLParameters(key){
+	    return (document.location.search.match(new RegExp("(?:^\\?|&)"+key+"=(.*?)(?=&|$)"))||['',null])[1];
+	}
 
- //     channel = getURLParameters("channel");
- //     if(channel == null) {
- //     	channel = "";
- //     }   
- //     if(channel != null && channel.length > 0 ) {
- //         apkURL = "http://hlj-app.b0.upaiyun.com/zmw/upload/android-package/app/" + channel + "/helijia.apk";
- //     }
+     channel = getURLParameters("channel");
+     if(channel == null) {
+     	channel = "";
+     }   
+     if(channel != null && channel.length > 0 ) {
+         // apkURL = "http://hlj-app.b0.upaiyun.com/zmw/upload/android-package/app/" + channel + "/helijia.apk";
+         apkURL = "http://cdnfile.itis6am.com/"+channel+"/qcrl.apk"
+     }else{
+ 		 apkURL = "http://cdnfile.itis6am.com/qcrl.apk"
+ 	}
